3ca65edd1da7c8e480db0703e88f982939af0a85
aliraza <chaudaryaliraza0@gmail.com>
2024-07-21 12:40:52 +0500

    #2 design the signin and register screens

--- src/screens/private.jsx
+++ src/screens/private.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/signin.scss";
 
 const Private = () => {
+  const [isRightPanelActive, setRightPanelActive] = useState(false);
+
+  const handleSignInClick = () => {
+    setRightPanelActive(false);
+  };
+
+  const handleSignUpClick = () => {
+    setRightPanelActive(true)
+  };
+
   return (
-    <div>Private</div>
+    <div className='wrapper'>
+      <div className={`container private-container ${isRightPanelActive ? "right-panel-active" : ""}`}>
+        {/* Sign Up */}
+        <div className="form-container signup-container">
+          <form action="#" className="form" id="form1">
+            <h2 className="private-heading">Private Account</h2>
+            <input type="text" placeholder="First Name" className="private-input" />
+            <input type="text" placeholder="Last Name" className="private-input" />
+            <input type="text" placeholder="dd/mm/yyyy DOB" className="private-input" />
+            <input type="number" placeholder="ID Card Number" className="private-input" />
+            <input type="email" placeholder="Email" className="private-input" />
+            <input type="number" placeholder="Ph #" className="private-input" />
+            <input type="password" placeholder="Password" className="private-input" />
+            <Link to="/" className="button private-btn">Sign Up</Link>
+          </form>
+        </div>
+
+        {/* Sign In */}
+        <div className="form-container signin-container">
+          <form action="#" className="form" id="form2">
+            <h2 className="private-heading">Business Account</h2>
+            <input type="text" placeholder="First Name" className="private-input" />
+            <input type="text" placeholder="Last Name" className="private-input" />
+            <input type="text" placeholder="dd/mm/yyyy DOB" className="private-input" />
+            <input type="number" placeholder="ID Card Number" className="private-input" />
+            <input type="email" placeholder="Email" className="private-input" />
+            <input type="number" placeholder="Ph #" className="private-input" />
+            <input type="password" placeholder="Password" className="private-input" />
+            <Link to="/" className="button1">Sign In</Link>
+          </form>
+        </div>
+
+        {/* OverLay */}
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <button className="button" id="signIn" onClick={handleSignInClick}>Business Account</button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <button className="button" id="signUp" onClick={handleSignUpClick}>Private Account</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,8 +1,84 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "../styles/signin.scss";
 
 const Signin = () => {
+
+  const handleSignInClick = () => {
+    const container = document.querySelector('.container');
+    container.classList.remove('right-panel-active');
+  };
+
+  const handleSignUpClick = () => {
+    const container = document.querySelector('.container');
+    container.classList.add('right-panel-active');
+  }
+
+  const signupFormSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log("signup data", email, password)
+  }
+
+  const signinFormSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    console.log("signin data", email, password)
+  }
+
   return (
-    <div>Signin</div>
+    <div className='wrapper'>
+      <div className='container right-panel-active'>
+
+        {/* SignUp */}
+        <div className='form-container signup-container'>
+          <form onSubmit={signupFormSubmit} action="" id='form1' className='form'>
+            <h2 className='heading'>Sign Up</h2>
+            <Link to="/private" className='button1'>Business Account</Link>
+            <Link to="/private" type='submit' className='button2'>Private Account</Link>
+          </form>
+        </div>
+
+        {/* SignIn */}
+        <div className='form-container signin-container'>
+          <form action="" onSubmit={signinFormSubmit} id='form2' className='form'>
+            <select className="language-select">
+              <option value="en">English</option>
+              <option value="fr">French</option>
+              <option value="es">Spanish</option>
+              <option value="de">German</option>
+              <option value="it">Italian</option>
+              <option value="pt">Portuguese</option>
+              <option value="ru">Russian</option>
+              <option value="zh">Chinese</option>
+              <option value="ja">Japanese</option>
+              <option value="ko">Korean</option>
+            </select>
+            <h2 className='heading'>Sign In</h2>
+            <input type="email" name='email' placeholder='Email' className='input' />
+            <input type="password" name='password' placeholder='Password' className='input' />
+            <a href="#" className='link'>Forgot your password?</a>
+            <Link to="/" type='submit' className='button signin-btn' >Sign In</Link>
+          </form>
+        </div>
+
+        {/* Overlay */}
+        <div className='overlay-container'>
+          <div className='overlay'>
+            <div className='overlay-panel overlay-left'>
+              <button className='button' id='signin' onClick={handleSignInClick}>Sign In</button>
+            </div>
+            <div className='overlay-panel overlay-right'>
+              <Link to="#" className='button' id='signup' onClick={handleSignUpClick}>Sign Up</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
